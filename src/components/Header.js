7d006857b188defb3d0a231f93852f4456a28ff4
Antonio Jumbo <tonn.raus09@gmail.com>
2020-11-19 12:08:37 +0100

    gravatart -PENDING- to check

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,48 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
+import gravatar from "../utils/gravatar";
 import "../assets/styles/components/Header.scss";
 import logo from "../assets/statics/logo-platzi-video-BW2.png";
 import userIcon from "../assets/statics/user-icon.png";
 
 // JSX syntax, returned value must go bettwen (element)
-const Header = () => (
-  <header className="header">
-    <Link to="/">
-      <img className="header__img" src={logo} alt="Platzi Video" />
-    </Link>
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src={userIcon} alt="" />
-        <p>Perfil</p>
+const Header = (props) => {
+  const { user } = props;
+  /* How many elements (keys) has object user */
+  const hasUser = Object.keys(user).lenght > 0;
+  return (
+    <header className="header">
+      <Link to="/">
+        <img className="header__img" src={logo} alt="Platzi Video" />
+      </Link>
+      <div className="header__menu">
+        <div className="header__menu--profile">
+          {hasUser ? (
+            <img src={gravatar(user.email)} alt={user.email} />
+          ) : (
+            <img src={userIcon} alt="" />
+          )}
+          <p>Perfil</p>
+        </div>
+        <ul>
+          <li>
+            <Link to="/login">Cuenta</Link>
+          </li>
+          <li>
+            <a href="/">Cerrar Sesión</a>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>
-          <Link to="/login">Cuenta</Link>
-        </li>
-        <li>
-          <a href="/">Cerrar Sesión</a>
-        </li>
-      </ul>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+export default connect(mapStateToProps, null)(Header);
---
+++ src/utils/gravatar.js
@@ -0,0 +1,11 @@
+import md5 from "md5";
+
+/* md5 can read @ so we encode it */
+const gravatar = (email) => {
+  const base = "https://gravatar.com/avatar/";
+  const formattedEmail = email.trim().toLowerCase();
+  const hash = md5(formattedEmail, { encoding: "binary" });
+  return `${base}${hash}`;
+};
+
+export default gravatar;
